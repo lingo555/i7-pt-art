ce7b1d8306fb75b93b87c7750bddfe4a0ec75eb1
lingo555 <lzxsj201210@gmail.com>
2024-10-19 18:23:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -194,6 +194,9 @@ function init() {
   calculateRemaining();
   filterButtons();
   logList.innerHTML = '';
+  document.querySelectorAll('input[name="adjust1"]').[3].checked = true;
+  document.querySelectorAll('input[name="adjust2"]').[5].checked = true;
+  document.querySelectorAll('input[name="adjust3"]').[5].checked = true;
 };
 
 // イベントリスナーの設定
